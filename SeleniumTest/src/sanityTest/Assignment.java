c7276efc7ada9df1d432a5eee3304c89a58087ad
maanvir <maanvir2611@gmail.com>
2016-06-18 11:46:36 -0400

    Back to basic one

--- SeleniumTest/src/sanityTest/Assignment.java
+++ SeleniumTest/src/sanityTest/Assignment.java
@@ -16,7 +16,6 @@ driver.findElement(By.linkText("LOGIN / REGISTER")).click();
 driver.findElement(By.xpath(".//*[@id='new-account-btn']")).click();
 //Select s = new Select(driver.findElement(By.xpath(".//*[@id='password']")));
 driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("maan");
-System.out.println("Welcome to the first commit");
 	}
 
 }
