187563c63745e34c992f6dc5ea7309733ac591e7
maanvir <maanvir2611@gmail.com>
2016-06-18 11:34:32 -0400

    First change to check commit command

--- SeleniumTest/src/sanityTest/Assignment.java
+++ SeleniumTest/src/sanityTest/Assignment.java
@@ -16,6 +16,7 @@ driver.findElement(By.linkText("LOGIN / REGISTER")).click();
 driver.findElement(By.xpath(".//*[@id='new-account-btn']")).click();
 //Select s = new Select(driver.findElement(By.xpath(".//*[@id='password']")));
 driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("maan");
+System.out.println("Welcome to the first commit");
 	}
 
 }
